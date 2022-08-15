dae3f0b6c9982790c9c17a4587812edfa2c56e3c
monokemonoke <kawabataryuhei@gmail.com>
2022-08-15 22:45:56 +0900

    use jsonify

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
     return 'hello world'
+
+@app.route('/about', methods=['GET'])
+def about():
+    return jsonify({"hello": "world"})
 
 if __name__ == '__main__':
     app.run(debug=True)
